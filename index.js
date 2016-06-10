019b96c8b46a80eb829373e7f0d03f6e694afd33
Jon Church <me@jonchurch.com>
2016-06-09 20:43:28 -0400

    added comments to the server code

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var server = express();
 
+//Add index page to /
 server.get('/', function(request, response){
 	response.sendFile('html/index.html', {root: __dirname + '/public/'});
 });
 
+//Add about page to /about/
 server.get('/about', function(request, response){
 	response.sendFile('html/about.html', {root: __dirname + '/public/'});
 });
 
+//Add contact page to /contact/
 server.get('/contact', function(request, response){
 	response.sendFile('html/contact.html', {root: __dirname + '/public/'});
 });
