6234983b8cf9f42d82aa207380068d50cd59b57a
Jon Church <me@jonchurch.com>
2016-06-09 20:09:46 -0400

    added about us page and link in express server

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ server.get('/', function(request, response){
 	response.sendFile('html/index.html', {root: __dirname + '/public/'});
 });
 
+server.get('/about', function(request, response){
+	response.sendFile('html/about.html', {root: __dirname + '/public/'});
+});
+
+
+
 
 server.listen(8080);
