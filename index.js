b9dc72aecdbf07a1fb437500d13498a777f75a68
Jon Church <me@jonchurch.com>
2016-06-13 20:21:48 -0400

    added body-parser and used it to respond with form data

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var server = express();
+var bodyParser = require('body-parser')
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 //Add index page to /
 server.get('/', function(request, response){
@@ -15,9 +19,9 @@ server.get('/about', function(request, response){
 server.get('/contact', function(request, response){
 	response.sendFile('html/contact.html', {root: __dirname + '/public/'});
 });
-
+//Add post method to contact page
 server.post('/contact', function(request, response){
-	response.send("hello");
+	response.json(request.body);
 });
 
 
